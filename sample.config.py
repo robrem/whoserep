e818127d19b18c6f02c0bf441f22124a37bf39d6
Rob Remington <robrem@uw.edu>
2017-05-02 12:42:08 -0700

    update sample.config.py to match config.py

--- sample.config.py
+++ sample.config.py
@@ -1,5 +1,11 @@
+# Twitter authentication
 TWITTER_ACCESS_TOKEN = ""
 TWITTER_ACCESS_TOKEN_SECRET = ""
 TWITTER_CONSUMER_KEY = ""
 TWITTER_CONSUMER_SECRET = ""
+
+# OpenSecrets.org authentication
 OPENSECRETS_API_KEY = ""
+
+# Should the bot log be written to a local file in the current directory?
+LOG_LOCAL = True
