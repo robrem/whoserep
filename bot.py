63de8d750bba4da8ca35c29f50c7cd936be0d4d4
Rob Remington <rob@D-108-179-163-243.dhcp4.washington.edu>
2017-05-04 12:05:12 -0700

    added DC to states list

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ logfile_name = bot_name + ".log"
 
 
 # ========= US State codes =================================================
-states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", 
+states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", 
           "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", 
           "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", 
           "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", 
