ecf600fb643146d5449dc84e4e2ecb00e28c39d2
Rob Remington <rob@D-140-142-255-57.dhcp4.washington.edu>
2017-05-05 15:54:51 -0700

    update sample.config.py to reflect new config setup

--- sample.config.py
+++ sample.config.py
@@ -1,11 +1,13 @@
-# Twitter authentication
-TWITTER_ACCESS_TOKEN = ""
-TWITTER_ACCESS_TOKEN_SECRET = ""
-TWITTER_CONSUMER_KEY = ""
-TWITTER_CONSUMER_SECRET = ""
+secrets = {
+    'TWITTER_ACCESS_TOKEN' : '',
+    'TWITTER_ACCESS_TOKEN_SECRET' : '',
+    'TWITTER_CONSUMER_KEY' : '',
+    'TWITTER_CONSUMER_SECRET' : '',
+    'OPENSECRETS_API_KEY' : '',
+    'PROPUBLICA_API_KEY' : ''
+}
 
-# OpenSecrets.org authentication
-OPENSECRETS_API_KEY = ""
-
-# Should the bot log be written to a local file in the current directory?
-LOG_LOCAL = True
+config_vars = {
+    # Should the bot log be written to a local file in the current directory?
+    'LOG_LOCAL' : True
+}
