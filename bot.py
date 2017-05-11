de8b2ccc5242920c5dbd1d6afdd971a243816070
Rob Remington <robrem@users.noreply.github.com>
2017-05-10 23:56:55 -0700

    bot.py: handle TweetText error by aborting tweet

--- bot.py
+++ bot.py
@@ -21,38 +21,44 @@ logfile_name = bot_name + ".log"
 
 
 def create_tweet():
-  """Create the tweet text"""
-  TT = TweetText()
-  return TT.get()
+    """Create the tweet text"""
+    try:
+        TT = TweetText()
+    except TweetTextError:
+        return None
+
+    return TT.get()
 
 
 def tweet(text):
-  """Tweet the text from the bot account"""
-  # Twitter auth
-  auth = tweepy.OAuthHandler(secrets['TWITTER_CONSUMER_KEY'], secrets['TWITTER_CONSUMER_SECRET'])
-  auth.set_access_token(secrets['TWITTER_ACCESS_TOKEN'], secrets['TWITTER_ACCESS_TOKEN_SECRET'])
-  api = tweepy.API(auth)
+    """Tweet the text from the bot account"""
+    # Twitter auth
+    auth = tweepy.OAuthHandler(secrets['TWITTER_CONSUMER_KEY'], secrets['TWITTER_CONSUMER_SECRET'])
+    auth.set_access_token(secrets['TWITTER_ACCESS_TOKEN'], secrets['TWITTER_ACCESS_TOKEN_SECRET'])
+    api = tweepy.API(auth)
 
-  try:
-    api.update_status(text)
-  except tweepy.TweepError as e:
-    log(e.message)
-  else:
-    log("Tweeted: " + text)
+    try:
+        api.update_status(text)
+    except tweepy.TweepError as e:
+        log(e.message)
+    else:
+        log("Tweeted: " + text)
 
 
 def log(message):
-  """Enter message in log file"""
-  if config_vars['LOG_LOCAL']:
-    path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-    with open(os.path.join(path, logfile_name), 'a+') as f:
-        t = strftime("%d %b %Y %H:%M:%S", gmtime())
-        f.write("\n" + t + " " + message)
-  else:
-    # Heroku prints stdout and stderr to its Logplex
-    print bot_name + ": " + message
+    """Enter message in log file"""
+    if config_vars['LOG_LOCAL']:
+        path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        with open(os.path.join(path, logfile_name), 'a+') as f:
+            t = strftime("%d %b %Y %H:%M:%S", gmtime())
+            f.write("\n" + t + " " + message)
+    else:
+        # Heroku prints stdout and stderr to its Logplex
+        print bot_name + ": " + message
 
 
 if __name__ == "__main__":
-  tweet_text = create_tweet()
-  tweet(tweet_text)
+    tweet_text = create_tweet()
+
+    if tweet_text:
+        tweet(tweet_text)
